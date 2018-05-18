1880b53db98de662e034aeb0213d00605a904344
Stephanie <stephaniejoychamblee@gmail.com>
2018-05-18 16:51:42 -0400

    Remove unused code

--- topics/router.js
+++ topics/router.js
@@ -64,37 +64,4 @@ router.post('/', (req, res) => {
     });
 });
 
-router.put('/:id', (req, res) => {
-  // ensure that the id in the request path and the one in request body match
-  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-    const message = (
-      `Request path id (${req.params.id}) and request body id ` +
-      `(${req.body.id}) must match`);
-    console.error(message);
-    return res.status(400).json({ message: message });
-  }
-
-  const toUpdate = {};
-  const updateableFields = ['topicName'];
-
-  updateableFields.forEach(field => {
-    if (field in req.body) {
-      toUpdate[field] = req.body[field];
-    }
-  });
-
-  Topic
-    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
-    .findByIdAndUpdate(req.params.id, { $set: toUpdate })
-    .then(topic => res.status(204).end())
-    .catch(err => res.status(500).json({ message: 'Internal server error' }));
-});
-
-router.delete('/:id', (req, res) => {
-  Topic
-    .findByIdAndRemove(req.params.id)
-    .then(topic => res.status(204).end())
-    .catch(err => res.status(500).json({ message: 'Internal server error' }));
-});
-
 module.exports = {router};
